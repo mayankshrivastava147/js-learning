d3fb604466acccceb4b96a2f9e752257a39624b5
mayankshrivastava147 <92111620+mayankshrivastava147@users.noreply.github.com>
2024-01-18 11:35:03 +0000

    array

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -41,24 +41,24 @@ const myArr2= new Array(1,2,3,4,5)
 const newArr = myArr.join()
 
 // console.log(myArr);
-// console.log(newArr);
-
+//  console.log(newArr);
+// console.log(typeof newArry);
 
 
 //*********************************************Slice, Splice************************************//
 
+//  console.log("A ", myArr);
+
+// const myn1 = myArr.slice(1,3)     // didnt aFFect original array
 
 
-const myn1 = myArr.slice(1,3)
+//  console.log("mynewArray",myn1);
+//  console.log("B ", myArr);
 
-// console.log("A ", myArr);
-// console.log(myn1);
-// console.log("B ", myArr);
+// const myn2 = myArr.splice(1,3)     //  affects original array
 
-const myn2 = myArr.splice(1,3)
-
-// console.log(myn2);
-// console.log("C ", myArr);
+//  console.log("mynewArray2",myn2);
+//  console.log("C ", myArr);
 
 
 
@@ -67,20 +67,20 @@ const dc_heros = ['superman','flash','batman']
 
 
 
-// marvel_heros.push(dc_heros)   // push on existing array
+//  marvel_heros.push(dc_heros)   // push on existing array
 
 // console.log(marvel_heros);
-// console.log(marvel_heros[3][2]);
+//  console.log(marvel_heros[3][2]);
 
 
 
 myNewheros = marvel_heros.concat(dc_heros)  // concat append and give new array
-// console.log(myNewheros);
+//console.log(myNewheros);
 
 
 // spread operator
-const all_new_heros = [...marvel_heros,...dc_heros]
-// console.log(all_new_heros);
+const all_new_heros = [...marvel_heros,...dc_heros]  // ... spreads arrays
+//console.log(all_new_heros);
 
 
 // indepth array solution 
@@ -108,9 +108,6 @@ let score3 = 300
 
 
 console.log(Array.of(score1,score2,score3));
-
-
-
 
 
 
